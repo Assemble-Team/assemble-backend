75a6eb25d1c85ccf0053a561b9cdeb35f92a12fb
ssongmina <thdalsdk5498@naver.com>
2026-01-17 22:52:06 +0900

    [FEAT] 모든 엔티티에 기본적으로 추가할 필드들 작성

--- src/main/java/assemble/api/ApiApplication.java
+++ src/main/java/assemble/api/ApiApplication.java
@@ -2,8 +2,11 @@ package assemble.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ApiApplication {
 
 	public static void main(String[] args) {
