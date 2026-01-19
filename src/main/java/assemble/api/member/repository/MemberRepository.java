3ef0786fa3a98a1f765252e82f4b45aa20578ef5
ssongmina <thdalsdk5498@naver.com>
2026-01-19 21:30:56 +0900

    [FEAT] 회원 리포지토리 작성

---
+++ src/main/java/assemble/api/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package assemble.api.member.repository;
+
+import assemble.api.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByEmail(String email);
+
+}
