c195f54c6a39e74a242d40b5b6405cd3b7a6056d
ssongmina <thdalsdk5498@naver.com>
2026-01-26 21:52:44 +0900

    [FEAT] 회원가입 및 로그인 시 발생하는 에러 상태 추가

--- src/main/java/assemble/api/apiPayload/status/MemberErrorStatus.java
+++ src/main/java/assemble/api/apiPayload/status/MemberErrorStatus.java
@@ -12,6 +12,8 @@ public enum MemberErrorStatus implements ErrorReason {
     WRONG_CODE(HttpStatus.BAD_REQUEST, "MEMBER4001", "이메일 인증코드가 올바르지 않습니다"),
     EXIST_EMAIL(HttpStatus.BAD_REQUEST, "MEMBER4002", "이미 존재하는 이메일 주소입니다"),
     NOT_EMAIL_VERIFY(HttpStatus.BAD_REQUEST, "MEMBER4003", "이메일 인증 정보가 없습니다. 다시 진행하세요"),
+    NOT_EXIST_EMAIL(HttpStatus.BAD_REQUEST, "MEMBER4004", "존재하지 않는 이메일 주소입니다"),
+    NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "MEMBER4005", "일치하지 않는 비밀번호입니다"),
     ;
 
     private final HttpStatus httpStatus;
