25530abd523d6c08df277a40d581cf68f557d4a6
ssongmina <thdalsdk5498@naver.com>
2026-01-27 23:54:40 +0900

    [FEAT] 소모임 생성에 필요한 DTO

---
+++ src/main/java/assemble/api/club/dto/ClubRequestDTO.java
@@ -0,0 +1,36 @@
+package assemble.api.club.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class ClubRequestDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CreateClubDTO{
+
+        @NotNull
+        String name;
+
+        @NotNull
+        String level;
+
+        @NotNull
+        String category;
+
+        @NotNull
+        String region;
+
+        Long maxNumber;
+
+        @NotNull
+        String description;
+
+        String imageUrl;
+    }
+}
---
+++ src/main/java/assemble/api/club/dto/ClubResponseDTO.java
@@ -0,0 +1,20 @@
+package assemble.api.club.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+public class ClubResponseDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CreateClubResultDTO{
+        Long clubId;
+        LocalDateTime createdAt;
+    }
+}
