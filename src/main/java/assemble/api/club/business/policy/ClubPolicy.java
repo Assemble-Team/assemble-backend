8aad9af12133937b5b72a3694e403ba2bd521bad
ssongmina <thdalsdk5498@naver.com>
2026-01-29 21:24:04 +0900

    [FEAT] 소모임을 수정할 권한이 있는지 확인하는 메서드 작성

--- src/main/java/assemble/api/club/business/policy/ClubPolicy.java
+++ src/main/java/assemble/api/club/business/policy/ClubPolicy.java
@@ -2,10 +2,21 @@ package assemble.api.club.business.policy;
 
 import assemble.api.apiPayload.handler.GeneralException;
 import assemble.api.apiPayload.status.ClubErrorStatus;
+import assemble.api.club.domain.Club;
 import assemble.api.club.domain.enums.DifficultyLevel;
+import assemble.api.club.domain.mapping.MemberClub;
+import assemble.api.club.repository.MemberClubRepository;
+import assemble.api.member.domain.Member;
 import assemble.api.member.domain.enums.InterestCategory;
+import assemble.api.member.domain.enums.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class ClubPolicy {
+
+    private final MemberClubRepository memberClubRepository;
 
     public static DifficultyLevel parseLevel(String level){
         if(level.equals("LOW")){
@@ -40,6 +51,14 @@ public class ClubPolicy {
         }
         else{
             throw new GeneralException(ClubErrorStatus.INVALID_INTEREST_CATEGORY);
+        }
+    }
+
+    public void validateUpdateInfo(Club club, Member member){
+        MemberClub memberClub = memberClubRepository.findByMemberAndClub(member, club)
+                .orElseThrow(() -> new GeneralException(ClubErrorStatus.NOT_JOIN_MEMBER_AND_CLUB));
+        if(memberClub.getRole() != Role.LEADER){
+            throw new GeneralException(ClubErrorStatus.UPDATE_PERMISSION_DENIED);
         }
     }
 }
