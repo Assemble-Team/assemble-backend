7e6d4461e4afc8fe2daefa102c00678bf940e923
ssongmina <thdalsdk5498@naver.com>
2026-01-30 23:17:00 +0900

    [FEAT] 소모임 목록 조회 결과용 DTO 및 컨버터 작성

--- src/main/java/assemble/api/club/converter/ClubConverter.java
+++ src/main/java/assemble/api/club/converter/ClubConverter.java
@@ -1,8 +1,11 @@
 package assemble.api.club.converter;
 
+import assemble.api.club.domain.Club;
 import assemble.api.club.dto.ClubResponseDTO;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 public class ClubConverter {
 
@@ -14,6 +17,45 @@ public class ClubConverter {
 
     public static ClubResponseDTO.ClubLikesResultDTO toClubLikesResultDTO(boolean liked){
         return ClubResponseDTO.ClubLikesResultDTO.builder()
+                .liked(liked)
+                .build();
+    }
+
+    public static ClubResponseDTO.ClubDetailResultDTO toClubDetailResultDTO(Club club, Long likesNum, boolean liked) {
+        return ClubResponseDTO.ClubDetailResultDTO.builder()
+                .clubName(club.getName())
+                .description(club.getDescription())
+                .level(club.getLevel())
+                .region(club.getRegion())
+                .category(club.getInterestCategory())
+                .imageUrl(club.getImageUrl())
+                .likesNum(likesNum)
+                .liked(liked)
+                .build();
+    }
+
+    public static ClubResponseDTO.FindClubListResultDTO toFindClubListResultDTO(List<Club> clubList, Set<Long> likedClubIds) {
+        List<ClubResponseDTO.FindClubResultDTO> result = clubList.stream()
+                .map(club -> toFindClubResultDTO(club, likedClubIds.contains(club.getId())
+                ))
+                .toList();
+        return ClubResponseDTO.FindClubListResultDTO.builder()
+                .list(result)
+                .build();
+    }
+
+    public static ClubResponseDTO.FindClubResultDTO toFindClubResultDTO(Club club, boolean liked) {
+        return ClubResponseDTO.FindClubResultDTO.builder()
+                .name(club.getName())
+                .imageUrl(club.getImageUrl())
+                .description(club.getDescription())
+                .category(club.getInterestCategory())
+                .level(club.getLevel())
+                .region(club.getRegion())
+                .status(club.getStatus())
+                .curNumbers(club.getCurNumbers())
+                .maxNumbers(club.getMaxNumbers())
+                .likes((long) club.getMemberLikesClubList().size())
                 .liked(liked)
                 .build();
     }
--- src/main/java/assemble/api/club/dto/ClubResponseDTO.java
+++ src/main/java/assemble/api/club/dto/ClubResponseDTO.java
@@ -1,11 +1,15 @@
 package assemble.api.club.dto;
 
+import assemble.api.club.domain.enums.ClubStatus;
+import assemble.api.club.domain.enums.DifficultyLevel;
+import assemble.api.member.domain.enums.InterestCategory;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ClubResponseDTO {
 
@@ -23,5 +27,46 @@ public class ClubResponseDTO {
     @AllArgsConstructor
     public static class ClubLikesResultDTO{
         boolean liked;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ClubDetailResultDTO{
+        private String clubName;
+        private String description;
+        private String region;
+        private String imageUrl;
+        private InterestCategory category;
+        private DifficultyLevel level;
+        private Long likesNum;
+        private boolean liked;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class FindClubListResultDTO{
+        private List<FindClubResultDTO> list;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class FindClubResultDTO{
+        private String name;
+        private String imageUrl;
+        private String description;
+        private InterestCategory category;
+        private DifficultyLevel level;
+        private String region;
+        private ClubStatus status;
+        private Long curNumbers;
+        private Long maxNumbers;
+        private Long likes;
+        private boolean liked;
     }
 }
