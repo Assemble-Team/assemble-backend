567d00bc9de045c44c91cade5ffc4f7c0141d617
ssongmina <thdalsdk5498@naver.com>
2026-01-30 18:05:20 +0900

    [FEAT] MembeLikesClub 수 count하는 메서드 작성

--- src/main/java/assemble/api/member/repository/MemberLikesClubRepository.java
+++ src/main/java/assemble/api/member/repository/MemberLikesClubRepository.java
@@ -3,9 +3,14 @@ package assemble.api.member.repository;
 import assemble.api.member.domain.mapping.MemberLikesClub;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberLikesClubRepository extends JpaRepository<MemberLikesClub, Long> {
 
     Optional<MemberLikesClub> findByMemberIdAndClubId(Long memberId, Long clubId);
+
+    Long countByMemberIdAndClubId(Long memberId, Long clubId);
+
+    Long countByClubId(Long clubId);
 }
