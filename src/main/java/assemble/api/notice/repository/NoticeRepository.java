f566858ed05a93b612cb0c34f1a12cb12e1736d0
ssongmina <thdalsdk5498@naver.com>
2026-02-01 22:41:29 +0900

    [FEAT] 소모임을 통해 공지사항 목록을 조회하는 메서드 작성

--- src/main/java/assemble/api/notice/repository/NoticeRepository.java
+++ src/main/java/assemble/api/notice/repository/NoticeRepository.java
@@ -1,7 +1,13 @@
 package assemble.api.notice.repository;
 
+import assemble.api.club.domain.Club;
 import assemble.api.notice.domain.Notice;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+
+    List<Notice> findByClubOrderByCreatedAtDesc(Club club, Pageable pageable);
 }
