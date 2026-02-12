9cadad6ece5002e223541f58717d36b4ee07e0ad
ssongmina <thdalsdk5498@naver.com>
2026-02-12 16:11:55 +0900

    [FEAT] 소모임의 일정에 대한 회원 참석 여부를 조회하는 메서드 작성

--- src/main/java/assemble/api/member/business/finder/MemberScheduleFinder.java
+++ src/main/java/assemble/api/member/business/finder/MemberScheduleFinder.java
@@ -5,8 +5,10 @@ import assemble.api.member.domain.mapping.MemberSchedule;
 import assemble.api.member.repository.MemberScheduleRepository;
 import assemble.api.schedule.domain.Schedule;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -17,5 +19,9 @@ public class MemberScheduleFinder {
 
     public Optional<MemberSchedule> findByMemberAndSchedule(Member member, Schedule schedule) {
         return memberScheduleRepository.findByMemberAndSchedule(member,schedule);
+    }
+
+    public List<MemberSchedule> findByMemberAndSchedules(Member member, Page<Schedule> scheduleList) {
+        return memberScheduleRepository.findByMemberAndScheduleIn(member, scheduleList.stream().toList());
     }
 }
