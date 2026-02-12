857205d62b54b468fed129c04bd524ce1b084991
ssongmina <thdalsdk5498@naver.com>
2026-02-12 16:11:08 +0900

    [FEAT] 회원일정 목록 조회 결과 DTO 및 컨버터 작성

--- src/main/java/assemble/api/schedule/dto/ScheduleResponseDTO.java
+++ src/main/java/assemble/api/schedule/dto/ScheduleResponseDTO.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class ScheduleResponseDTO {
 
@@ -20,6 +24,28 @@ public class ScheduleResponseDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class AttendScheduleResultDTO{
+        boolean attend;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GetScheduleListResultDTO{
+        List<GetScheduleResultDTO> list;
+        int totalPages;
+        int page;
+        int size;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GetScheduleResultDTO{
+        LocalDateTime startAt;
+        String title;
+        String location;
         boolean attend;
     }
 }
