2a24ec9217253771ad8976adc9d589542a2397af
ssongmina <thdalsdk5498@naver.com>
2026-02-12 16:11:45 +0900

    [FEAT] 소모임의 일정을 조회하는 메서드 작성

--- src/main/java/assemble/api/schedule/business/finder/ScheduleFinder.java
+++ src/main/java/assemble/api/schedule/business/finder/ScheduleFinder.java
@@ -2,10 +2,17 @@ package assemble.api.schedule.business.finder;
 
 import assemble.api.apiPayload.handler.GeneralException;
 import assemble.api.apiPayload.status.ScheduleErrorStatus;
+import assemble.api.club.domain.Club;
 import assemble.api.schedule.domain.Schedule;
 import assemble.api.schedule.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -16,5 +23,9 @@ public class ScheduleFinder {
     public Schedule findByScheduleId(Long scheduleId) {
         return scheduleRepository.findById(scheduleId)
                 .orElseThrow(()-> new GeneralException(ScheduleErrorStatus.NOT_EXIST_SCHEDULE));
+    }
+
+    public Page<Schedule> findByClubAndStartAtAfter(Club club, LocalDateTime now, Pageable pageable) {
+        return scheduleRepository.findByClubAndStartAtAfter(club, now, pageable);
     }
 }
--- src/main/java/assemble/api/schedule/repository/ScheduleRepository.java
+++ src/main/java/assemble/api/schedule/repository/ScheduleRepository.java
@@ -1,7 +1,16 @@
 package assemble.api.schedule.repository;
 
+import assemble.api.club.domain.Club;
 import assemble.api.schedule.domain.Schedule;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    Page<Schedule> findByClubAndStartAtAfter(Club club, LocalDateTime localDate, Pageable pageable);
 }
