6219fa628c07a0596d8e817b115d22e1aa1a2617
ssongmina <thdalsdk5498@naver.com>
2026-02-14 01:44:40 +0900

    [FEAT] 모임 신청 상태 변경 메서드 작성

--- src/main/java/assemble/api/club/domain/mapping/ClubJoinRequest.java
+++ src/main/java/assemble/api/club/domain/mapping/ClubJoinRequest.java
@@ -31,4 +31,8 @@ public class ClubJoinRequest extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    public void updateStatus(JoinStatus status){
+        this.status = status;
+    }
 }
