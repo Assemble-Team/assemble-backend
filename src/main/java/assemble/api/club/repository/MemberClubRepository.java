186d6b0da285614ff4f15c64568528c321815045
ssongmina <thdalsdk5498@naver.com>
2026-02-14 23:35:46 +0900

    [FEAT] 소모임_회원을 조회하는 메서드 작성

--- src/main/java/assemble/api/club/repository/MemberClubRepository.java
+++ src/main/java/assemble/api/club/repository/MemberClubRepository.java
@@ -3,6 +3,9 @@ package assemble.api.club.repository;
 import assemble.api.club.domain.Club;
 import assemble.api.club.domain.mapping.MemberClub;
 import assemble.api.member.domain.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -15,4 +18,7 @@ public interface MemberClubRepository extends JpaRepository<MemberClub, Long> {
     List<MemberClub> findByClub(Club club);
 
     boolean existsByMemberAndClub(Member member, Club club);
+
+    @EntityGraph(attributePaths = {"member"})
+    Page<MemberClub> findByClub(Club club, Pageable pageable);
 }
