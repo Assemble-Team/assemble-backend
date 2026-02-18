5ccb5f1e95da8f905a0923ec8efc3b994afa86ca
ssongmina <thdalsdk5498@naver.com>
2026-02-18 23:21:27 +0900

    [FIX] jwt 인증 필터 로직 수정

--- src/main/java/assemble/api/auth/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/assemble/api/auth/jwt/filter/JwtAuthenticationFilter.java
@@ -22,7 +22,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = jwtTokenProvider.resolveToken(request);
-        if(token != null && jwtTokenProvider.validateToken(token)){
+        if(token != null){
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
