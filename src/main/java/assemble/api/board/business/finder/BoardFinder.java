72332d5c889e484d86bdd275abef86e7095ec1c3
ssongmina <thdalsdk5498@naver.com>
2026-03-05 23:06:01 +0900

    [FEAT] 소모임으로 게시판 찾는 메서드 작성

--- src/main/java/assemble/api/board/business/finder/BoardFinder.java
+++ src/main/java/assemble/api/board/business/finder/BoardFinder.java
@@ -1,5 +1,7 @@
 package assemble.api.board.business.finder;
 
+import assemble.api.apiPayload.handler.GeneralException;
+import assemble.api.apiPayload.status.BoardErrorStatus;
 import assemble.api.board.domain.Board;
 import assemble.api.board.repository.BoardRepository;
 import assemble.api.club.domain.Club;
@@ -16,5 +18,10 @@ public class BoardFinder {
 
     public Page<Board> findByClub(Club club, Pageable page) {
         return boardRepository.findBoardsByClub(club, page);
+    }
+
+    public Board findByClub(Club club) {
+        return boardRepository.findBoardByClub(club)
+                .orElseThrow(() -> new GeneralException(BoardErrorStatus.NOT_EXIST_BOARD));
     }
 }
--- src/main/java/assemble/api/board/repository/BoardRepository.java
+++ src/main/java/assemble/api/board/repository/BoardRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     Page<Board> findBoardsByClub(Club club, Pageable page);
+
+    Optional<Board> findBoardByClub(Club club);
 }
